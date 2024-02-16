8d4df5bb3aba4b55e5f3dfce644686b774bc101d
justtshivam <sg8501110@gmail.com>
2024-02-16 23:57:06 +0530

    my 3rd git code push

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -4,8 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "SUDHANSHU";
-		String m = "Kakkar";
+		String s = "janmayjay";
+		String m = "singh";
 		
 		System.out.println(s+m);
 	}
