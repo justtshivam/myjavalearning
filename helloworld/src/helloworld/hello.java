0ba22858ba24be5c81aec5ae3c518b27fd9fa7fb
justtshivam <sg8501110@gmail.com>
2024-02-16 23:54:37 +0530

    my changes 2nd

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -4,8 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "shivam";
-		String m = "gupta";
+		String s = "SUDHANSHU";
+		String m = "Kakkar";
 		
 		System.out.println(s+m);
 	}
